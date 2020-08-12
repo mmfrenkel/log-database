f0eeaea510eb6fa9727fda04d181afd78b7ac9c2
mmfrenkel <megan.frenkel@gmail.com>
2020-08-12 11:40:33 -0400

    adding dummy main file

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "two_three_tree.h"
+
+
+int main(argc, *argv[]) {
+
+	printf("For an interactive program...");
+
+	TwoThreeTree *tthree = malloc(sizeof(TwoThreeTree));
+}
